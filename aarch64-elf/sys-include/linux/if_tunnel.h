c522c5bceb89649a39e1ec099744b8d30dbc4086
aepranata <aepranata@gmail.com>
2023-12-21 11:18:56 +0700

    Import ARM64 GCC 20231221
    Build completed on: Thu Dec 21 11:18:56 WIB 2023
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/__w/gcc-build/gcc-arm64/bin/../libexec/gcc/aarch64-elf/14.0.0/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf --disable-decimal-float --disable-docs --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --enable-default-ssp --enable-languages=c,c++,fortran --enable-threads=posix --prefix=/__w/gcc-build/gcc-build/../gcc-arm64 --with-gnu-as --with-gnu-ld --with-headers=/usr/include --with-linker-hash-style=gnu --with-newlib --with-pkgversion='Atiga GCC' --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib zstd
    gcc version 14.0.0 20231221 (Bleeding Edge) (Atiga GCC)
    
    LLD: LLD 18.0.0 (compatible with GNU linkers)
    Signed-off-by: aepranata <aepranata@gmail.com>

--- aarch64-elf/sys-include/linux/if_tunnel.h
+++ aarch64-elf/sys-include/linux/if_tunnel.h
@@ -77,10 +77,6 @@ enum {
 	IFLA_IPTUN_ENCAP_DPORT,
 	IFLA_IPTUN_COLLECT_METADATA,
 	IFLA_IPTUN_FWMARK,
-
-	__IFLA_IPTUN_VENDOR_BREAK, /* Ensure new entries do not hit the below. */
-	IFLA_IPTUN_FAN_MAP = 33,
-
 	__IFLA_IPTUN_MAX,
 };
 #define IFLA_IPTUN_MAX	(__IFLA_IPTUN_MAX - 1)
@@ -180,23 +176,10 @@ enum {
 #define TUNNEL_VXLAN_OPT	__cpu_to_be16(0x1000)
 #define TUNNEL_NOCACHE		__cpu_to_be16(0x2000)
 #define TUNNEL_ERSPAN_OPT	__cpu_to_be16(0x4000)
+#define TUNNEL_GTP_OPT		__cpu_to_be16(0x8000)
 
 #define TUNNEL_OPTIONS_PRESENT \
-		(TUNNEL_GENEVE_OPT | TUNNEL_VXLAN_OPT | TUNNEL_ERSPAN_OPT)
-
-enum {
-	IFLA_FAN_UNSPEC,
-	IFLA_FAN_MAPPING,
-	__IFLA_FAN_MAX,
-};
-
-#define IFLA_FAN_MAX (__IFLA_FAN_MAX - 1)
-
-struct ifla_fan_map {
-	__be32		underlay;
-	__be32		overlay;
-	__u16		underlay_prefix;
-	__u16		overlay_prefix;
-};
+		(TUNNEL_GENEVE_OPT | TUNNEL_VXLAN_OPT | TUNNEL_ERSPAN_OPT | \
+		TUNNEL_GTP_OPT)
 
 #endif /* _IF_TUNNEL_H_ */
